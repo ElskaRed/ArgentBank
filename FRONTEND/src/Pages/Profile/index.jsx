7450e5c12b5e80698f9adceeaccb36473a066047
Roxanne Vignon <vignon.roxanne@gmail.com>
2023-12-03 10:20:41 +0100

    Division en components

--- FRONTEND/src/Pages/Profile/index.jsx
+++ FRONTEND/src/Pages/Profile/index.jsx
@@ -1,3 +1,5 @@
+import Account from '../../Components/Account';
+
 function Profile() {
 	return (
 		<main className="main bg-dark">
@@ -10,36 +12,10 @@ function Profile() {
 				<button className="edit-button">Edit Name</button>
 			</div>
 			<h2 className="sr-only">Accounts</h2>
-			<section className="account">
-				<div className="account-content-wrapper">
-					<h3 className="account-title">Argent Bank Checking (x8349)</h3>
-					<p className="account-amount">$2,082.79</p>
-					<p className="account-amount-description">Available Balance</p>
-				</div>
-				<div className="account-content-wrapper cta">
-					<button className="transaction-button">View transactions</button>
-				</div>
-			</section>
-			<section className="account">
-				<div className="account-content-wrapper">
-					<h3 className="account-title">Argent Bank Savings (x6712)</h3>
-					<p className="account-amount">$10,928.42</p>
-					<p className="account-amount-description">Available Balance</p>
-				</div>
-				<div className="account-content-wrapper cta">
-					<button className="transaction-button">View transactions</button>
-				</div>
-			</section>
-			<section className="account">
-				<div className="account-content-wrapper">
-					<h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-					<p className="account-amount">$184.30</p>
-					<p className="account-amount-description">Current Balance</p>
-				</div>
-				<div className="account-content-wrapper cta">
-					<button className="transaction-button">View transactions</button>
-				</div>
-			</section>
+			{/* Pas encore de props passés sur les Account car login non implémenté. */}
+			<Account />
+			<Account />
+			<Account />
 		</main>
 	);
 }
